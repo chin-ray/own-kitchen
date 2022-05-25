121a2ef268a4385f3478851ca8af697d21d66935
chinray <1745874297@qq.com>
2022-05-25 16:20:49 +0800

    【feat】更新首页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>Header</div>
-  <div>Content</div>
+  <div class="page-wrapper scroll-y">
+    <div class="page-header">Header</div>
+    <div class="page-content">
+      <h3>Content</h3>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,3 +13,25 @@ export default {
 };
 </script>
 <script setup></script>
+
+<style scoped lang="scss">
+$colorHeader: #f2f2f2;
+$colorContent: #fcfcfc;
+$contentBorderRadius: 15px;
+
+.page-wrapper {
+}
+.page-header {
+  height: 160px;
+  padding: $globalPadding;
+  background-color: $colorHeader;
+}
+.page-content {
+  height: 1000px;
+  padding: $globalPadding;
+  background-color: $colorContent;
+  border-top-left-radius: $contentBorderRadius;
+  border-top-right-radius: $contentBorderRadius;
+  margin-top: calc(0px - #{$contentBorderRadius});
+}
+</style>
