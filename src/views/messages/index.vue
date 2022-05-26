9a66b11db78287abeb2cff886eebbc15094f701c
chinray <1745874297@qq.com>
2022-05-26 16:09:21 +0800

    【feat】页面调整

--- src/views/messages/index.vue
+++ src/views/messages/index.vue
@@ -1,4 +1,11 @@
-<template>消息</template>
+<template>
+  <div class="global-padding">
+    <div class="page-header">Header</div>
+    <div class="page-content">
+      <h3>Content</h3>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
