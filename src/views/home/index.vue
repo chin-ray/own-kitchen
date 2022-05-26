7b671afa76647c095cb2a85631bf51de4ae4e8a3
chinray <1745874297@qq.com>
2022-05-26 16:09:13 +0800

    【feat】页面调整

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page-wrapper scroll-y">
-    <div class="page-header">Header</div>
-    <div class="page-content">
+    <div class="page-header global-padding">Header</div>
+    <div class="page-content global-padding">
       <h3>Content</h3>
     </div>
   </div>
@@ -23,12 +23,10 @@ $contentBorderRadius: 15px;
 }
 .page-header {
   height: 160px;
-  padding: $globalPadding;
   background-color: $colorHeader;
 }
 .page-content {
   height: 1000px;
-  padding: $globalPadding;
   background-color: $colorContent;
   border-top-left-radius: $contentBorderRadius;
   border-top-right-radius: $contentBorderRadius;
