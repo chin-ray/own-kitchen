0879b5086207086603e56e29cc2123be109e2814
chinray <1745874297@qq.com>
2022-05-27 09:21:21 +0800

    【feat】页面调整

---
+++ src/views/messages/detail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page-header">
+    <kit-navbar title="聊天群组"></kit-navbar>
+  </div>
+  <div class="page-content global-padding">
+    <h3>Content</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MessagesDetail",
+};
+</script>
